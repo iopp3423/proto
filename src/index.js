fb03ef797fde40b337e4ae6d4c2b2f329e69ad3c
jun <48996701+iopp3423@users.noreply.github.com>
2022-11-21 00:03:18 +0900

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Store from "./Store"
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store = {Store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+reportWebVitals();
